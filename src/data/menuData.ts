b6e0ac4b760c9f3b58bbc97856ddd3c78b7a6c83
haz-pqh <hazpiqah@gmail.com>
2026-08-30 21:35:35 +0800

    Update menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -347,7 +347,7 @@ export const MENU_ITEMS: MenuItem[] = [
     price: 53.90,
     originalPrice: 57.00,
     category: 'combos',
-    image: '/bucket.png',
+    image: '/bucketSpecial.png',
     isBestSeller: true,
     isChefSpecial: true,
     spiceLevel: 2,
